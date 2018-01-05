edcfa56377c514d5dfb80a662e512169894528dc
Juntao Qiu <juntao.qiu@gmail.com>
2018-01-05 22:17:03 +0800

    show 16 blend mode

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('#file-upload')
 const container = document.querySelector('#container')
+const modes = document.querySelectorAll('.thumb')
 
 const renderImage = (file) => {
 	const reader = new FileReader()
@@ -7,6 +8,12 @@ const renderImage = (file) => {
 	reader.onload = (e) => {
 		const url = e.target.result
 		container.style.setProperty('background-image', 'url("'+url+'")')
+
+		for(let i = 0, len = modes.length; i < len; i++) {
+			const mode = modes[i]
+			const text = mode.innerText
+			mode.style.setProperty('background-image', 'url("'+url+'")')
+		}
 	}
 
 	reader.readAsDataURL(file)
@@ -16,3 +23,8 @@ input.addEventListener('change', (e) => {
 	renderImage(e.target.files[0])
 })
 
+for(let i = 0, len = modes.length; i < len; i++) {
+	const mode = modes[i]
+	const text = mode.innerText
+	mode.style.setProperty('background-blend-mode', text)
+}
