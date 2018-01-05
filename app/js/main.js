b4249c116b970dcbc08170bee86ca6b66e791469
Juntao Qiu <juntao.qiu@gmail.com>
2018-01-05 22:28:28 +0800

    color picker for background

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,16 @@ const input = document.querySelector('#file-upload')
 const container = document.querySelector('#container')
 const modes = document.querySelectorAll('.thumb')
 
+const updateBackgroundColor = (value) => {
+	container.style.setProperty('background-color', value)
+
+	for(let i = 0, len = modes.length; i < len; i++) {
+		const mode = modes[i]
+		const text = mode.innerText
+		mode.style.setProperty('background-color', value)
+	}
+}
+
 const renderImage = (file) => {
 	const reader = new FileReader()
 
@@ -28,3 +38,8 @@ for(let i = 0, len = modes.length; i < len; i++) {
 	const text = mode.innerText
 	mode.style.setProperty('background-blend-mode', text)
 }
+
+const colorPicker = document.querySelector('#color')
+colorPicker.addEventListener('change', (e) => {
+	updateBackgroundColor(e.target.value)
+})
